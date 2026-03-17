db025d3aa73dc5756f3a189da3404dc628cc7eb1
Tochi8128 <toch2718@gmail.com>
2026-03-18 01:10:17 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+async function loadWorks() {
+  const nav = document.getElementById("works-nav");
+
+  try {
+    const res = await fetch("/tochi2718/data/works.json");
+    const data = await res.json();
+
+    nav.innerHTML = data.works.map(work => `
+      <div class="nav-item">
+        <img src="${work.thumbnail}" alt="${work.title}">
+      </div>
+    `).join("");
+
+  } catch (e) {
+    console.error("worksの読み込み失敗", e);
+  }
+}
+
+loadWorks();
